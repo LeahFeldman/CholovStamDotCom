b53eb4eff3988f73d7089322525cdade99511975
Chana Leah Nissel <clnissel@wits.edu>
2025-05-11 18:57:26 -0400

    SearchBar component

--- RecipeSearch/src/SearchBar.jsx
+++ RecipeSearch/src/SearchBar.jsx
@@ -2,25 +2,45 @@ import { useState, useEffect } from 'react';
 
 function SearchBar(){
     const [query, setQuery] = useState("");
-    useEffect(({ newQuery }) => {
-        setQuery(`${ newQuery }`)
-    });
+    const [data, setData] = useState([]);
+
+    useEffect(() => {                               
+        if (query){
+            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+            .then(res => res.json())
+            .then(data => setData(data.meals));
+        }
+    }, [query]);
 
     
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log('Submitted query:', inputValue);
-        useEffect(event.target.value);
-    };
+        const inputValue = event.target.elements.searchInput.value;
+        setQuery(inputValue);
+        console.log('Submitted query:', query);
+    }
 
-   
     return(
         <>
             <form onSubmit={handleSubmit}>
-                <input type="text" required= {true} placeholder="search a recipe..."/>
+                <input 
+                    type="text" 
+                    required= {true} 
+                    name="searchInput" 
+                    value={query} 
+                    placeholder="search a recipe..."/>
             </form>
 
-            <Result                 />
+            <ul>
+                {data
+
+                    //data is left with meals that match the query
+                    .filter(item => item.strMeal.toLowerCase().includes(query.toLowerCase()))
+
+                    .map(item => (
+                    <Result key={item.idMeal} item={item}/>
+                ))}
+            </ul>
         </>
     );
 }
