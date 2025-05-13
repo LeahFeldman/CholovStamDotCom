51b10f7519e3ce91cc8f6ef5496f9c2de59da6d6
Chana Leah Nissel <clnissel@wits.edu>
2025-05-13 11:52:45 -0400

    added Result component and styes

---
+++ RecipeSearch/src/Result.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from 'Result.module.css';
+
+
+function Result({ item }){
+
+    const navigate=useNavigate();
+
+    const handleButton = () => {
+        navigate(`/Details/${item.idMeal}`);
+    };
+
+    return(
+        <div className={styles.resultsContainer}>
+            <button className={styles.resultItem} onClick={handleButton}>{ item.strMeal }</button>
+        </div>
+    );
+};
+
+export default Result;
