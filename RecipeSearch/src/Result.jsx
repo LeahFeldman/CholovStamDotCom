19a9fdc7da08cb3d05cbda9c49a934f944321319
Chana Leah Nissel <clnissel@wits.edu>
2025-05-13 12:06:41 -0400

    fixed import statement

--- RecipeSearch/src/Result.jsx
+++ RecipeSearch/src/Result.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from 'Result.module.css';
+import styles from './Result.module.css';
 
 
 function Result({ item }){
