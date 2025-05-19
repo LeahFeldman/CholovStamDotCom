8181ee57421345effca331f215c3bcb232e9b16b
leahfeldmanlf@gmail.com <leahfeldmanlf@gmail.com>
2025-05-19 17:44:21 -0400

    fixed About us and back button functionality

--- RecipeSearch/src/pages/BackButton.jsx
+++ RecipeSearch/src/pages/BackButton.jsx
@@ -1,11 +1,12 @@
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styles from './BackButton.module.css';
 
 export function BackButton() {
-    function goback() {
-        Navigate(-1);
-    }
+    const navigate = useNavigate();
+    function goback () {
+        navigate(-1);
+    };
     return (
-        <button className={styles.backbutton} onClick = "goback()" >Back</button>
-    )
-}    
+        <button className={styles.backbutton} onClick={goback}>Back</button>
+    );
+};    
